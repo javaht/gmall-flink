ba0c3ecd8172c4ab35d04307101132f34de58169
zhouhengtao <java_ht@163.com>
2022-06-27 17:10:19 +0800

    no

--- gmall-realtime/src/main/java/com/zht/app/dim/DimApp.java
+++ gmall-realtime/src/main/java/com/zht/app/dim/DimApp.java
@@ -1,13 +1,21 @@
 package com.zht.app.dim;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.google.gson.JsonObject;
 import com.zht.utils.KafkaUtils;
 import org.apache.flink.api.common.restartstrategy.RestartStrategies;
 import org.apache.flink.api.common.time.Time;
 import org.apache.flink.runtime.state.hashmap.HashMapStateBackend;
 import org.apache.flink.streaming.api.CheckpointingMode;
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.CheckpointConfig;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.ProcessFunction;
+import org.apache.flink.util.Collector;
+import org.apache.flink.util.OutputTag;
 
 
 import java.util.concurrent.TimeUnit;
@@ -33,8 +41,26 @@ public class DimApp {
         DataStreamSource<String> gmallDS  = env.addSource(KafkaUtils.getKafkaConsumer(topic, groupid));
 
 
-        //过滤json格式的数据
+        //定义脏数据
+        OutputTag<String> dirty = new OutputTag<>("dirty");
 
+        //过滤json格式的数据
+        SingleOutputStreamOperator<JSONObject> jsonObj = gmallDS.process(new ProcessFunction<String, JSONObject>() {
+
+            @Override
+            public void processElement(String value, ProcessFunction<String, JSONObject>.Context ctx, Collector<JSONObject> out) throws Exception {
+                try {
+                    JSONObject jsonObject = JSON.parseObject(value);
+                    out.collect(jsonObject);
+                } catch (Exception e) {
+                    ctx.output(dirty, value);
+                }
+            }
+        });
+
+         //取出脏数据并打印
+        DataStream<String> sideOutput = jsonObj.getSideOutput(dirty);
+        sideOutput.print("sideOutput>>>>>>>>>>>>>");
 
         //使用flinkcdc  读取mysql配置信息
 
