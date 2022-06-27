9dfffd44e50d24aa8cdfe4a29e3d6e335faf88f0
root <java_ht@163.com>
2022-06-27 22:47:07 +0800

    go on

--- gmall-realtime/src/main/java/com/zht/app/dim/DimApp.java
+++ gmall-realtime/src/main/java/com/zht/app/dim/DimApp.java
@@ -2,18 +2,23 @@ package com.zht.app.dim;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
-import com.google.gson.JsonObject;
+import com.ververica.cdc.connectors.mysql.source.MySqlSource;
+import com.ververica.cdc.connectors.mysql.source.MySqlSourceBuilder;
+import com.ververica.cdc.connectors.mysql.table.StartupOptions;
+import com.ververica.cdc.debezium.JsonDebeziumDeserializationSchema;
+import com.zht.bean.TableProcess;
 import com.zht.utils.KafkaUtils;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.restartstrategy.RestartStrategies;
+import org.apache.flink.api.common.state.MapStateDescriptor;
 import org.apache.flink.api.common.time.Time;
 import org.apache.flink.runtime.state.hashmap.HashMapStateBackend;
 import org.apache.flink.streaming.api.CheckpointingMode;
-import org.apache.flink.streaming.api.datastream.DataStream;
-import org.apache.flink.streaming.api.datastream.DataStreamSource;
-import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.datastream.*;
 import org.apache.flink.streaming.api.environment.CheckpointConfig;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.ProcessFunction;
+import org.apache.flink.streaming.api.functions.co.BroadcastProcessFunction;
 import org.apache.flink.util.Collector;
 import org.apache.flink.util.OutputTag;
 
@@ -39,37 +44,53 @@ public class DimApp {
          String topic = "topic_db";
          String groupid = "dim_sink_app";
         DataStreamSource<String> gmallDS  = env.addSource(KafkaUtils.getKafkaConsumer(topic, groupid));
-
-
-        //定义脏数据
-        OutputTag<String> dirty = new OutputTag<>("dirty");
-
+        OutputTag<String> dirtyTag = new OutputTag<String>("dirty");
         //过滤json格式的数据
         SingleOutputStreamOperator<JSONObject> jsonObj = gmallDS.process(new ProcessFunction<String, JSONObject>() {
-
             @Override
             public void processElement(String value, ProcessFunction<String, JSONObject>.Context ctx, Collector<JSONObject> out) throws Exception {
                 try {
                     JSONObject jsonObject = JSON.parseObject(value);
                     out.collect(jsonObject);
                 } catch (Exception e) {
-                    ctx.output(dirty, value);
+                    ctx.output(dirtyTag, value);
                 }
             }
         });
-
-         //取出脏数据并打印
-        DataStream<String> sideOutput = jsonObj.getSideOutput(dirty);
-        sideOutput.print("sideOutput>>>>>>>>>>>>>");
-
+        DataStream<String> sideOutput = jsonObj.getSideOutput(dirtyTag);
+        sideOutput.print("Dirty>>>>>>>>>>");
         //使用flinkcdc  读取mysql配置信息
+
+        MySqlSource<String> mySqlSource = MySqlSource.<String>builder()
+                 .hostname("hadoop102").port(3306)
+                .username("root").password("123456").databaseList("gmall_config").tableList("gmall_config.table_process")
+                .deserializer(new JsonDebeziumDeserializationSchema()).startupOptions(StartupOptions.initial()).build();
+        DataStreamSource<String> mysqlSourceDs = env.fromSource(mySqlSource, WatermarkStrategy.noWatermarks(), "MysqlSource");
+
 
         //将配置信息处理成广播
 
+        MapStateDescriptor<String, TableProcess> mapState = new MapStateDescriptor<>("mapState", String.class, TableProcess.class);//状态描述器
+        BroadcastStream<String> broadcastStream = mysqlSourceDs.broadcast(mapState); //广播流
+
+
         //连接两个流
+        BroadcastConnectedStream<JSONObject, String> connectedStream = jsonObj.connect(broadcastStream);
 
 
-       // 根据广播数据疏离主流数据
+
+        // 根据广播数据疏离主流数据
+        connectedStream.process(new BroadcastProcessFunction<JSONObject, String, JSONObject>() {
+            @Override
+            public void processElement(JSONObject value, BroadcastProcessFunction<JSONObject, String, JSONObject>.ReadOnlyContext ctx, Collector<JSONObject> out) throws Exception {
+
+            }
+
+            @Override
+            public void processBroadcastElement(String value, BroadcastProcessFunction<JSONObject, String, JSONObject>.Context ctx, Collector<JSONObject> out) throws Exception {
+
+            }
+        });
 
 
          //将数据写入phoenix
