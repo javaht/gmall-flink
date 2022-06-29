f60713e7cc2f7e903924a2e63b42a6a6d4056197
root <java_ht@163.com>
2022-06-30 00:26:48 +0800

    进度go on

--- gmall-realtime/src/main/java/com/zht/app/dim/DimApp.java
+++ gmall-realtime/src/main/java/com/zht/app/dim/DimApp.java
@@ -4,31 +4,34 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONException;
 import com.alibaba.fastjson.JSONObject;
 import com.ververica.cdc.connectors.mysql.source.MySqlSource;
-import com.ververica.cdc.connectors.mysql.source.MySqlSourceBuilder;
 import com.ververica.cdc.connectors.mysql.table.StartupOptions;
 import com.ververica.cdc.debezium.JsonDebeziumDeserializationSchema;
 import com.zht.app.func.TableProcessFunction;
 import com.zht.bean.TableProcess;
+import com.zht.common.GmallConfig;
 import com.zht.utils.KafkaUtils;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
-import org.apache.flink.api.common.restartstrategy.RestartStrategies;
+import org.apache.flink.api.common.state.BroadcastState;
 import org.apache.flink.api.common.state.MapStateDescriptor;
-import org.apache.flink.api.common.time.Time;
-import org.apache.flink.runtime.state.hashmap.HashMapStateBackend;
-import org.apache.flink.streaming.api.CheckpointingMode;
+import org.apache.flink.api.common.state.ReadOnlyBroadcastState;
 import org.apache.flink.streaming.api.datastream.*;
-import org.apache.flink.streaming.api.environment.CheckpointConfig;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.ProcessFunction;
 import org.apache.flink.streaming.api.functions.co.BroadcastProcessFunction;
 import org.apache.flink.util.Collector;
 import org.apache.flink.util.OutputTag;
-
-
-import java.util.concurrent.TimeUnit;
+import javax.security.auth.login.Configuration;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 public class DimApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
          //获取执行环境
         StreamExecutionEnvironment  env = new StreamExecutionEnvironment();
@@ -46,8 +49,8 @@ public class DimApp {
          String topic = "topic_db";
          String groupid = "dim_sink_app";
         DataStreamSource<String> gmallDS  = env.addSource(KafkaUtils.getKafkaConsumer(topic, groupid));
-        OutputTag<String> dirtyTag = new OutputTag<String>("dirty");
 
+        OutputTag<String> dirtyTag = new OutputTag<String>("dirty");
         //过滤json格式的数据
         SingleOutputStreamOperator<JSONObject> jsonObj = gmallDS.process(new ProcessFunction<String, JSONObject>() {
             @Override
@@ -62,33 +65,34 @@ public class DimApp {
         });
         DataStream<String> sideOutput = jsonObj.getSideOutput(dirtyTag);
         sideOutput.print("Dirty>>>>>>>>>>");
-        //使用flinkcdc  读取mysql配置信息
 
+        //使用flinkcdc  读取mysql配置信息
         MySqlSource<String> mySqlSource = MySqlSource.<String>builder()
                  .hostname("hadoop102").port(3306)
                 .username("root").password("123456").databaseList("gmall_config").tableList("gmall_config.table_process")
                 .deserializer(new JsonDebeziumDeserializationSchema()).startupOptions(StartupOptions.initial()).build();
         DataStreamSource<String> mysqlSourceDs = env.fromSource(mySqlSource, WatermarkStrategy.noWatermarks(), "MysqlSource");
-
-
-        //将配置信息处理成广播
-
-        MapStateDescriptor<String, TableProcess> mapState = new MapStateDescriptor<>("mapState", String.class, TableProcess.class);//状态描述器
-        BroadcastStream<String> broadcastStream = mysqlSourceDs.broadcast(mapState); //广播流
+        //将配置信息处理成广
+        MapStateDescriptor<String, TableProcess> mapStateDescriptor = new MapStateDescriptor<>("mapState", String.class, TableProcess.class);//状态描述器
+        BroadcastStream<String> broadcastStream = mysqlSourceDs.broadcast(mapStateDescriptor); //广播流
 
 
         //连接两个流
         BroadcastConnectedStream<JSONObject, String> connectedStream = jsonObj.connect(broadcastStream);
 
 
+        // 根据广播数据处理主流数据
+        SingleOutputStreamOperator<JSONObject> hbaseDS = connectedStream.process(new TableProcessFunction(mapStateDescriptor));
 
-        // 根据广播数据疏离主流数据
-        connectedStream.process();
+        //将数据写出到phoenix中
+        hbaseDS.print(">>>>>>>>>");
+
+        //启动任务
+        env.execute("Dimapp");
 
 
-
-         //将数据写入phoenix
-
-        //启动
     }
+
+
+
 }
--- gmall-realtime/src/main/java/com/zht/app/func/TableProcessFunction.java
+++ gmall-realtime/src/main/java/com/zht/app/func/TableProcessFunction.java
@@ -1,11 +1,13 @@
 package com.zht.app.func;
 
 
-
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.zht.bean.TableProcess;
 import com.zht.common.GmallConfig;
-import net.minidev.json.JSONObject;
+import org.apache.flink.api.common.state.BroadcastState;
+import org.apache.flink.api.common.state.MapStateDescriptor;
+import org.apache.flink.api.common.state.ReadOnlyBroadcastState;
 import org.apache.flink.streaming.api.functions.co.BroadcastProcessFunction;
 import org.apache.flink.util.Collector;
 
@@ -14,86 +16,108 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
 
 public class TableProcessFunction extends BroadcastProcessFunction<JSONObject,String,JSONObject> {
 
+    private MapStateDescriptor<String,TableProcess> stateDescriptor;
     private Connection connection;
 
-
-    private void open(Configuration parameters) throws SQLException {
-        connection = DriverManager.getConnection(GmallConfig.PHOENIX_SERVER);
+    public TableProcessFunction(MapStateDescriptor<String, TableProcess> stateDescriptor) {
+        this.stateDescriptor = stateDescriptor;
     }
 
-    @Override
-    public void processBroadcastElement(String value, Context ctx, Collector<JSONObject> out) throws Exception {
 
-         //获取并解析数据为javabean对象
-        String after = JSON.parseObject(value).getString("after");
-        TableProcess tableProcess = JSON.parseObject(after, TableProcess.class);
-        //校验表是否存在
-        checkTable(tableProcess.getSinkTable(),tableProcess.getSinkColumns(),tableProcess.getSinkPk(),tableProcess.getSinkExtend());
-        //将数据写入状态
+        private void open(Configuration parameters) throws SQLException {
+            connection = DriverManager.getConnection(GmallConfig.PHOENIX_SERVER);
+        }
 
-
-    }
-        /*
-        * 在Phoniex中校验并创建表
-        * */
-    private void checkTable(String sinkTable, String sinkColumns, String sinkPk, String sinkExtend) throws SQLException {
-        PreparedStatement preparedStatement =null;
-        try {
-            if(sinkPk ==null ||sinkPk.equals("")){
-                sinkPk="id";
-            }
-            if(sinkExtend==null){
-                sinkExtend="";
-            }
-            StringBuilder sql = new StringBuilder("create table if not exists").append(GmallConfig.HBASE_SCHEMA).append(".").append(sinkTable).append("(");
-            String[] columns = sinkColumns.split(",");
-            for (int i = 0; i < columns.length; i++) {
-                String column = columns[i];
-                //判断是否为主键
-                if (!sinkPk.equals(column)) {
-                    sql.append(column).append("varchar");
-                } else {
-                    sql.append(column).append(" varchar primary key");
-                }
-
-                //判断不是最后一个字段
-                if(i<columns.length -1){
-                    sql.append(",");
-                }
-            }
-            sql.append(")").append(sinkExtend);
-            System.out.println(sql);
-              preparedStatement = connection.prepareStatement(sql.toString());
-              preparedStatement.execute();
-
-        } catch (SQLException e) {
-          throw new RuntimeException("建表"+sinkTable+"失败");
-        }finally {
-            if(preparedStatement!=null){
-                try {
-                    preparedStatement.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
+        @Override
+        public void processBroadcastElement(String value, BroadcastProcessFunction<JSONObject, String, JSONObject>.Context ctx, Collector<JSONObject> out) throws Exception {
+            //获取并解析数据为javabean对象
+            String after = JSON.parseObject(value).getString("after");
+            TableProcess tableProcess = JSON.parseObject(after, TableProcess.class);
+            //校验表是否存在
+            checkTable(tableProcess.getSinkTable(),tableProcess.getSinkColumns(),tableProcess.getSinkPk(),tableProcess.getSinkExtend());
+            //将数据写入状态
+            String key = tableProcess.getSourceTable();
+            BroadcastState<String, TableProcess> broadcastState = ctx.getBroadcastState(stateDescriptor);
+            broadcastState.put(key,tableProcess);
 
         }
 
+        @Override
+        public void processElement(JSONObject value, BroadcastProcessFunction<JSONObject, String, JSONObject>.ReadOnlyContext ctx, Collector<JSONObject> out) throws Exception {
+            //获取广播的配置数
+            ReadOnlyBroadcastState<String, TableProcess> broadcastState = ctx.getBroadcastState(stateDescriptor);
+            TableProcess tableProcess = broadcastState.get(value.getString("table"));
+            String type = value.getString("type");
+            if(tableProcess!=null && (("bootstrap-insert").equals(type)||("insert").equals(type)||("update").equals(type))){
+                //根据sinkcolums配置信息过滤字段
+                filter(value.getJSONObject("data"),tableProcess.getSinkColumns());
+                //补充sinktable输出
+                value.put("sinkTable",tableProcess.getSinkTable());
+                out.collect(value);
+            }else{
+                System.out.println("过滤掉"+value);
+            }
+        }
 
+        /*
+         * 过滤
+         * */
+        private  void filter(JSONObject data, String sinkColumns) {
+            String[] split = sinkColumns.split(",");
+            List<String> columnsList = Arrays.asList(split);
+            data.entrySet().removeIf(next -> !columnsList.contains(next.getKey()));
+        }
+
+        /*
+         * 在Phoniex中校验并创建表
+         * */
+        private void checkTable(String sinkTable, String sinkColumns, String sinkPk, String sinkExtend) throws SQLException {
+            PreparedStatement preparedStatement =null;
+            try {
+                if(sinkPk ==null ||sinkPk.equals("")){
+                    sinkPk="id";
+                }
+                if(sinkExtend==null){
+                    sinkExtend="";
+                }
+                StringBuilder sql = new StringBuilder("create table if not exists").append(GmallConfig.HBASE_SCHEMA).append(".").append(sinkTable).append("(");
+                String[] columns = sinkColumns.split(",");
+                for (int i = 0; i < columns.length; i++) {
+                    String column = columns[i];
+                    //判断是否为主键
+                    if (!sinkPk.equals(column)) {
+                        sql.append(column).append("varchar");
+                    } else {
+                        sql.append(column).append(" varchar primary key");
+                    }
+
+                    //判断不是最后一个字段
+                    if(i<columns.length -1){
+                        sql.append(",");
+                    }
+                }
+                sql.append(")").append(sinkExtend);
+                System.out.println(sql);
+                preparedStatement = connection.prepareStatement(sql.toString());
+                preparedStatement.execute();
+
+            } catch (SQLException e) {
+                throw new RuntimeException("建表"+sinkTable+"失败");
+            }finally {
+                if(preparedStatement!=null){
+                    try {
+                        preparedStatement.close();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
     }
 
-    @Override
-    public void processElement(JSONObject value, BroadcastProcessFunction<JSONObject, String, JSONObject>.ReadOnlyContext ctx, Collector<JSONObject> out) throws Exception {
 
-        //获取广播的配置数据
-
-        //根据sinkcolums配置信息过滤字段
-
-        //补充sinktable输出
-    }
-
-
-}
