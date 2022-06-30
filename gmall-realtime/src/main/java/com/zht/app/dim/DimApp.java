d2c527e908cbca3bc2a11b4a50ec0bf1f6f17a1b
zhouhengtao <java_ht@163.com>
2022-06-30 09:51:51 +0800

    go on

--- gmall-realtime/src/main/java/com/zht/app/dim/DimApp.java
+++ gmall-realtime/src/main/java/com/zht/app/dim/DimApp.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONException;
 import com.alibaba.fastjson.JSONObject;
 import com.ververica.cdc.connectors.mysql.source.MySqlSource;
+import com.ververica.cdc.connectors.mysql.source.MySqlSourceBuilder;
 import com.ververica.cdc.connectors.mysql.table.StartupOptions;
 import com.ververica.cdc.debezium.JsonDebeziumDeserializationSchema;
 import com.zht.app.func.TableProcessFunction;
@@ -11,15 +12,23 @@ import com.zht.bean.TableProcess;
 import com.zht.common.GmallConfig;
 import com.zht.utils.KafkaUtils;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.restartstrategy.RestartStrategies;
 import org.apache.flink.api.common.state.BroadcastState;
 import org.apache.flink.api.common.state.MapStateDescriptor;
+import org.apache.flink.api.common.time.Time;
+import org.apache.flink.runtime.state.hashmap.HashMapStateBackend;
+import org.apache.flink.streaming.api.CheckpointingMode;
 import org.apache.flink.api.common.state.ReadOnlyBroadcastState;
 import org.apache.flink.streaming.api.datastream.*;
+import org.apache.flink.streaming.api.environment.CheckpointConfig;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.ProcessFunction;
 import org.apache.flink.streaming.api.functions.co.BroadcastProcessFunction;
 import org.apache.flink.util.Collector;
 import org.apache.flink.util.OutputTag;
+
+
+import java.util.concurrent.TimeUnit;
 import javax.security.auth.login.Configuration;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -33,9 +42,9 @@ import java.util.Map;
 public class DimApp {
     public static void main(String[] args) throws Exception {
 
-         //获取执行环境
-        StreamExecutionEnvironment  env = new StreamExecutionEnvironment();
-        env.setParallelism(4);
+            //获取执行环境
+            StreamExecutionEnvironment  env = new StreamExecutionEnvironment();
+            env.setParallelism(4);
 /*      env.enableCheckpointing(3000L, CheckpointingMode.EXACTLY_ONCE);
         env.getCheckpointConfig().setCheckpointTimeout(60 * 1000L);
         env.getCheckpointConfig().setMinPauseBetweenCheckpoints(3000L);
@@ -45,54 +54,58 @@ public class DimApp {
         env.getCheckpointConfig().setCheckpointStorage("hdfs://hadoop102:8020/gmall/ck");
         System.setProperty("HADOOP_USER_NAME", "atguigu");
  */
-        //获取kafka的数据
-         String topic = "topic_db";
-         String groupid = "dim_sink_app";
-        DataStreamSource<String> gmallDS  = env.addSource(KafkaUtils.getKafkaConsumer(topic, groupid));
+            //获取kafka的数据
+            String topic = "topic_db";
+            String groupid = "dim_sink_app";
+            DataStreamSource<String> gmallDS  = env.addSource(KafkaUtils.getKafkaConsumer(topic, groupid));
 
-        OutputTag<String> dirtyTag = new OutputTag<String>("dirty");
-        //过滤json格式的数据
-        SingleOutputStreamOperator<JSONObject> jsonObj = gmallDS.process(new ProcessFunction<String, JSONObject>() {
-            @Override
-            public void processElement(String value, ProcessFunction<String, JSONObject>.Context ctx, Collector<JSONObject> out) throws Exception {
-                try {
-                    JSONObject jsonObject = JSON.parseObject(value);
-                    out.collect(jsonObject);
-                } catch (JSONException e) {
-                    ctx.output(dirtyTag, value);
+            OutputTag<String> dirtyTag = new OutputTag<String>("dirty");
+
+            //过滤json格式的数据
+            SingleOutputStreamOperator<JSONObject> jsonObj = gmallDS.process(new ProcessFunction<String, JSONObject>() {
+                @Override
+                public void processElement(String value, ProcessFunction<String, JSONObject>.Context ctx, Collector<JSONObject> out) throws Exception {
+                    try {
+                        JSONObject jsonObject = JSON.parseObject(value);
+                        out.collect(jsonObject);
+                    } catch (JSONException e) {
+                        ctx.output(dirtyTag, value);
+                    }
                 }
-            }
-        });
-        DataStream<String> sideOutput = jsonObj.getSideOutput(dirtyTag);
-        sideOutput.print("Dirty>>>>>>>>>>");
+            });
+            DataStream<String> sideOutput = jsonObj.getSideOutput(dirtyTag);
+            sideOutput.print("Dirty>>>>>>>>>>");
 
-        //使用flinkcdc  读取mysql配置信息
-        MySqlSource<String> mySqlSource = MySqlSource.<String>builder()
-                 .hostname("hadoop102").port(3306)
-                .username("root").password("123456").databaseList("gmall_config").tableList("gmall_config.table_process")
-                .deserializer(new JsonDebeziumDeserializationSchema()).startupOptions(StartupOptions.initial()).build();
-        DataStreamSource<String> mysqlSourceDs = env.fromSource(mySqlSource, WatermarkStrategy.noWatermarks(), "MysqlSource");
-        //将配置信息处理成广
-        MapStateDescriptor<String, TableProcess> mapStateDescriptor = new MapStateDescriptor<>("mapState", String.class, TableProcess.class);//状态描述器
-        BroadcastStream<String> broadcastStream = mysqlSourceDs.broadcast(mapStateDescriptor); //广播流
+            //使用flinkcdc  读取mysql配置信息
+
+            MySqlSource<String> mySqlSource = MySqlSource.<String>builder()
+                    .hostname("hadoop102").port(3306)
+                    .username("root").password("123456").databaseList("gmall_config").tableList("gmall_config.table_process")
+                    .deserializer(new JsonDebeziumDeserializationSchema()).startupOptions(StartupOptions.initial()).build();
+            DataStreamSource<String> mysqlSourceDs = env.fromSource(mySqlSource, WatermarkStrategy.noWatermarks(), "MysqlSource");
 
 
-        //连接两个流
-        BroadcastConnectedStream<JSONObject, String> connectedStream = jsonObj.connect(broadcastStream);
+            //将配置信息处理成广播
+
+            MapStateDescriptor<String, TableProcess> mapStateDescriptor = new MapStateDescriptor<>("mapState", String.class, TableProcess.class);//状态描述器
+            BroadcastStream<String> broadcastStream = mysqlSourceDs.broadcast(mapStateDescriptor); //广播流
 
 
-        // 根据广播数据处理主流数据
-        SingleOutputStreamOperator<JSONObject> hbaseDS = connectedStream.process(new TableProcessFunction(mapStateDescriptor));
+            //连接两个流
+            BroadcastConnectedStream<JSONObject, String> connectedStream = jsonObj.connect(broadcastStream);
 
-        //将数据写出到phoenix中
-        hbaseDS.print(">>>>>>>>>");
 
-        //启动任务
-        env.execute("Dimapp");
+            // 根据广播数据处理主流数据
+            SingleOutputStreamOperator<JSONObject> hbaseDS = connectedStream.process(new TableProcessFunction(mapStateDescriptor));
+
+            //将数据写出到phoenix中
+            hbaseDS.print(">>>>>>>>>");
+
+            //启动任务
+            env.execute("Dimapp");
+        }
+
 
 
     }
 
-
-
-}
