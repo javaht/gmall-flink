c1b11648e4b80110aead6f110433ecdb1a8f70ab
root <java_ht@163.com>
2022-07-19 23:14:48 +0800

    流量域版本-渠道-地区-访客类别粒度页面浏览各窗口汇总表

--- gmall-realtime/src/main/java/com/zht/app/dws/DwsTrafficVcChArIsNewPageViewWindow.java
+++ gmall-realtime/src/main/java/com/zht/app/dws/DwsTrafficVcChArIsNewPageViewWindow.java
@@ -1,6 +1,8 @@
 package com.zht.app.dws;
 
 
+import com.zht.utils.MyKafkaUtil;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 
@@ -18,17 +20,22 @@ public class DwsTrafficVcChArIsNewPageViewWindow {
 //        env.enableCheckpointing(3000L, CheckpointingMode.EXACTLY_ONCE);
 //        env.getCheckpointConfig().setCheckpointTimeout(60 * 1000L);
 //        env.getCheckpointConfig().setMinPauseBetweenCheckpoints(3000L);
-//        env.getCheckpointConfig().enableExternalizedCheckpoints(
-//                CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION
-//        );
-//        env.setRestartStrategy(RestartStrategies.failureRateRestart(
-//                3, Time.days(1), Time.minutes(1)
-//        ));
+//        env.getCheckpointConfig().enableExternalizedCheckpoints(CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION);
+//        env.setRestartStrategy(RestartStrategies.failureRateRestart(3, Time.days(1), Time.minutes(1)));
 //        env.setStateBackend(new HashMapStateBackend());
-//        env.getCheckpointConfig().setCheckpointStorage(
-//                "hdfs://hadoop102:8020/ck"
-//        );
-//        System.setProperty("HADOOP_USER_NAME", "atguigu");
+//        env.getCheckpointConfig().setCheckpointStorage("hdfs://hadoop102:8020/ck");
+//        System.setProperty("HADOOP_USER_NAME", "root");
+
+        //读取3个主题的数据创建三个流
+        String page_topic = "dwd_traffic_page_log";
+        String uv_topic = "dwd_traffic_unique_visitor_detail";
+        String uj_topic = "dwd_traffic_user_jump_detail";
+        String groupId = "dws_traffic_vc_ch_ar_isnew_page_view_window";
+
+        DataStreamSource<String> pageStringDS = env.addSource(MyKafkaUtil.getKafkaConsumer(page_topic, groupId));
+        DataStreamSource<String> uvStringDS = env.addSource(MyKafkaUtil.getKafkaConsumer(uv_topic, groupId));
+        DataStreamSource<String> ujStringDS = env.addSource(MyKafkaUtil.getKafkaConsumer(uj_topic, groupId));
+
 
 
 
