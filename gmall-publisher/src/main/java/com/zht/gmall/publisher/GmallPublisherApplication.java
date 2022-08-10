7cc7b364f75bebab761db7ef6bb6d8e7ae63632a
root <java_ht@163.com>
2022-08-10 22:18:04 +0800

    go  on

---
+++ gmall-publisher/src/main/java/com/zht/gmall/publisher/GmallPublisherApplication.java
@@ -0,0 +1,15 @@
+package com.zht.gmall.publisher;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan(basePackages = "com.zht.gmallpublisher.mapper")
+public class GmallPublisherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmallPublisherApplication.class, args);
+    }
+
+}
